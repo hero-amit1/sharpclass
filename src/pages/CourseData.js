78276248f5fa217adce0fceb8722dfbd21c3bdda
cloudsnepalweb33 <cloudsnepalweb33@gmail.com>
2026-05-19 19:04:41 +0545

    price remove

--- src/pages/CourseData.js
+++ src/pages/CourseData.js
@@ -15,15 +15,12 @@ const courseData = [
 
     duration: "6 Months",
 
-    price: "NPR 15,000",
+    
 
     mode: ["Online", "Physical"],
 
     image:
       "https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc?auto=format&fit=crop&w=1200&q=80",
-
-    video:
-      "https://www.youtube.com/embed/XubOlWcoF9w?start=191",
 
     syllabus: [
       "Engineering Mathematics",
@@ -59,15 +56,12 @@ const courseData = [
 
     duration: "5 Months",
 
-    price: "NPR 12,000",
 
     mode: ["Online", "Physical"],
 
     image:
       "https://images.unsplash.com/photo-1531297484001-80022131f5a1?auto=format&fit=crop&w=1200&q=80",
 
-    video:
-      "https://www.youtube.com/embed/XubOlWcoF9w?start=191",
 
     syllabus: [
       "Basic Engineering Concepts",
@@ -102,15 +96,13 @@ const courseData = [
 
     duration: "4 Months",
 
-    price: "NPR 10,000",
+
 
     mode: ["Online", "Physical"],
 
     image:
       "https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=1200&q=80",
 
-    video:
-      "https://www.youtube.com/embed/XubOlWcoF9w?start=191",
 
     syllabus: [
       "Fundamentals of Engineering",
@@ -145,16 +137,14 @@ const courseData = [
 
     duration: "3 Months",
 
-    price: "NPR 8,000",
+
 
     mode: ["Online", "Physical"],
 
     image:
       "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&w=1200&q=80",
 
-    video:
-      "https://www.youtube.com/embed/XubOlWcoF9w?start=191",
-
+   
     syllabus: [
       "Licensing Exam Preparation",
       "Past Questions Practice",
@@ -189,15 +179,14 @@ const courseData = [
 
     duration: "4 Months",
 
-    price: "NPR 9,000",
+   
 
     mode: ["Online", "Physical"],
 
     image:
       "https://images.unsplash.com/photo-1581094794329-c8112a89af12?auto=format&fit=crop&w=1200&q=80",
 
-    video:
-      "https://www.youtube.com/embed/XubOlWcoF9w?start=191",
+   
 
     syllabus: [
       "Basic Engineering Concepts",
